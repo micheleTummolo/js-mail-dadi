325a50e6e2fb1db98b059e254b1aec59791b151c
micheleTummolo <tummolomichele02@gmail.com>
2022-11-30 17:49:19 +0100

    Done script and structure

---
+++ mail/js/script.js
@@ -0,0 +1,35 @@
+const mail = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com", "mail4@gmail.com",]
+
+let user_mail;
+let access = false;
+const button = document.getElementById ('get_value');
+const reset = document.getElementById ('reset')
+
+button.addEventListener ('click', function(){
+    
+    user_mail = document.getElementById('mail_user').value;
+    console.log("Mail utente: " + user_mail);
+
+    for (let i=0; i < mail.length; i++){
+        console.log(mail[i])
+        if (mail[i] == user_mail){
+            access = true;
+        }
+    }
+    
+    console.log("Esito controllo accesso: " + access)
+    
+    if (access) {
+        document.getElementById ('output').innerHTML = "PUOI ACCEDERE";
+    }
+    else {
+        document.getElementById ('output').innerHTML = "NON PUOI ACCEDERE";
+    }
+
+    
+})
+
+reset.addEventListener ('click', function(){
+    document.getElementById("form_mail").reset();
+    document.getElementById ('output').innerHTML = "";
+})
